c2cdd1c7872bc6147124b54a8178cc5ebf7ca7f6
semihbkgr <buyukgungorsemih@gmail.com>
2024-06-23 01:13:20 +0300

    sdl2 for rendering

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,64 @@
-use winit::application::ApplicationHandler;
-use winit::event::{ElementState, KeyEvent, StartCause, WindowEvent};
-use winit::event_loop::{ActiveEventLoop, EventLoop};
-use winit::keyboard::Key;
-use winit::window::{Window, WindowId};
+use std::time::Duration;
 
-fn main() -> Result<(), impl std::error::Error> {
-    let event_loop = EventLoop::new().unwrap();
+use raycasting::RayCasting;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::Point;
 
-    let mut app = App::default();
-    event_loop.run_app(&mut app)
-}
+mod raycasting;
 
-#[derive(Default)]
-struct App {
-    close_requested: bool,
-    window: Option<Window>,
-}
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let window = video_subsystem
+        .window("Ray Tracing", 800, 600)
+        .position_centered()
+        .build()
+        .map_err(|e| e.to_string())?;
+    let mut canvas = window
+        .into_canvas()
+        .software()
+        .build()
+        .map_err(|e| e.to_string())?;
 
-impl ApplicationHandler for App {
-    fn new_events(&mut self, _event_loop: &ActiveEventLoop, cause: StartCause) {
-        println!("new_events: {cause:?}");
-    }
+    let mut raycasting = RayCasting::default();
 
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        let window_attributes = Window::default_attributes().with_title("Ray Tracing");
-        self.window = Some(event_loop.create_window(window_attributes).unwrap());
-    }
-
-    fn window_event(
-        &mut self,
-        _event_loop: &ActiveEventLoop,
-        _window_id: WindowId,
-        event: WindowEvent,
-    ) {
-        match event {
-            WindowEvent::CloseRequested => {
-                println!("close requested");
-                self.close_requested = true;
-            }
-            WindowEvent::KeyboardInput {
-                event:
-                    KeyEvent {
-                        logical_key: key,
-                        state: ElementState::Pressed,
-                        ..
-                    },
-                ..
-            } => match key.as_ref() {
-                Key::Character("1") => {
-                    println!("key pressed: 1")
+    'mainloop: loop {
+        for event in sdl_context.event_pump()?.poll_iter() {
+            match event {
+                Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
                 }
-                _ => (),
-            },
-            WindowEvent::RedrawRequested => {
-                println!("redraw request");
-                let window = self.window.as_ref().unwrap();
-                window.pre_present_notify();
+                | Event::Quit { .. } => break 'mainloop,
+                _ => {}
             }
-            _ => (),
         }
+
+        let lines = raycasting.lines(Duration::ZERO, 800, 600);
+
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+
+        for line in lines {
+            let color = match line.2 {
+                1 => Color::RED,
+                2 => Color::GREEN,
+                3 => Color::BLUE,
+                4 => Color::WHITE,
+                _ => Color::YELLOW,
+            };
+            canvas.set_draw_color(color);
+            canvas
+                .draw_line(
+                    Point::new(line.0.x as i32, line.0.y as i32),
+                    Point::new(line.1.x as i32, line.1.y as i32),
+                )
+                .unwrap();
+        }
+        canvas.present();
     }
 
-    fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
-        if self.close_requested {
-            event_loop.exit();
-        }
-    }
+    Ok(())
 }
